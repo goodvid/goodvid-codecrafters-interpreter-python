9b74660b233de06dd9260691a922f4fdc2abf25a
goodviv <vuppalav@purdue.edu>
2024-08-10 01:14:23 -0700

    1.7

--- app/main.py
+++ app/main.py
@@ -22,9 +22,12 @@ def main():
     # read per line, then read per token
     single_tokens = {';': 'SEMICOLON','-': 'MINUS','{' : 'LEFT_BRACE', '}': 'RIGHT_BRACE','(': 'LEFT_PAREN', ')': 'RIGHT_PAREN', '*': 'STAR', '.': 'DOT', ',': 'COMMA', '+': 'PLUS'}
     for line in file_contents:
-      for c in line:
+      for i, c in line:
         if c in single_tokens:
           print(single_tokens[c] + ' ' + c + ' null')
+        else:
+          print(f'[line {i}] Error: Unexpected character: {c}')
+          
     print('EOF  null')
 if __name__ == "__main__":
     main()
